25e27af8aa2985a190b33c5df02aa02d5dc4fffa
ArturNedava <121451856+ArturNedava@users.noreply.github.com>
2024-02-06 19:16:10 +0100

    first commit

---
+++ js/dragon.js
@@ -0,0 +1,36 @@
+// const dino = document.querySelector(".Dino")
+// const cactus = document.querySelector(".cactus")
+
+// document.addEventListener("click", function(event){
+//     jump();
+//     console.log("вы прыгнули")
+// })
+
+// function jump () {
+//    if (dino.classList != "jump") {
+//     dino.classList.add("jump")
+//    }
+
+
+//    setTimeout( function(){
+//     dino.classList.remove("jump")
+//    }, 3000)
+// }
+
+
+// let isAlive = setInterval ( function(){
+//     let DinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"))
+//     let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"))
+
+//     if (cactusLeft < 490 && cactusLeft > 440 - 40 && DinoTop >= 1590) {
+//         alert("Game Over")
+        
+//     }
+// }, 10)
+
+
+
+
+
+
+
